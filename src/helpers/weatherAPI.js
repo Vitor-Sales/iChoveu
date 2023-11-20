fcb44385997def6a2d43e2de3ffaa14011b118bf
Vitor Sales <vitormalisa@gmail.com>
2023-11-20 17:54:00 -0300

    function searchCities passing tests

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,14 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+  const arrayCities = await fetch(URL);
+  const response = await arrayCities.json();
+  if (response.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
+    return [];
+  }
+  return response;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
